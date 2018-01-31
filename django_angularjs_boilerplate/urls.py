e11671cfad0789b491087e22224dcf2917a30c15
Gabriel Fajardo <gfajardo95@gmail.com>
2018-01-30 19:52:14 -0500

    Initial commit

---
+++ django_angularjs_boilerplate/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.conf.urls import url
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.generic import TemplateView
+
+urlpatterns = [
+  url(r'admin/', admin.site.urls),
+  url(r'^$', ensure_csrf_cookie(TemplateView.as_view(template_name="index.html"))),
+]
