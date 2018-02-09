341a2a6aef5368dd029dd697260b3c55c665d9a9
Gabriel Fajardo <gfajardo95@gmail.com>
2018-02-08 19:59:11 -0500

    create url pattern for the index page

--- django_angularjs_boilerplate/urls.py
+++ django_angularjs_boilerplate/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
 
